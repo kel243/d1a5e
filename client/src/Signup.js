21bd79a5e71d3f106424905275e952d5f7bd8fc1
Ken Lin <kel117@pitt.edu>
2022-04-22 09:38:15 -0400

    signup page

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -2,25 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import {
   Grid,
-  Box,
   Typography,
   Button,
   FormControl,
   TextField,
   FormHelperText,
+  FormLabel,
 } from '@material-ui/core';
 import AuthLayout from './components/Layout/AuthLayout';
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(() => ({
-  authSwitcher: {
-    position: "fixed",
-    top: "2rem",
-    right: "3rem",
-    gap: "1rem"
-  }
-}));
-
+import useStyles from "./components/Layout/AuthPageStyles";
 
 const Signup = ({ user, register }) => {
   const history = useHistory();
@@ -53,66 +43,59 @@ const Signup = ({ user, register }) => {
     <AuthLayout>
       <Grid container direction="column" justifyContent="center" alignItems="center">
         <Grid className={classes.authSwitcher} container item alignItems='center' justifyContent='flex-end'>
-          <Typography>Already have an account?</Typography>
-          <Link href="/login" to="/login">
-            <Button>Login</Button>
+          <Typography className={classes.greyText}>Already have an account?</Typography>
+          <Link href="/login" to="/login" className={classes.linkText}>
+            <Button className={classes.linkButton}>Login</Button>
           </Link>
         </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
+        <form className={classes.form} onSubmit={handleRegister}>
+          <Grid className={classes.formGrid} container direction='column' alignItems='center'>
+            <Typography className={classes.headingText} component="h1">Create an account.</Typography>
+            <FormControl margin="normal" className={classes.fullWidth}>
+              <FormLabel>Username</FormLabel>
+              <TextField
+                aria-label="username"
+                name="username"
+                type="text"
+                required
+              />
+            </FormControl>
+            <FormControl margin="normal" className={classes.fullWidth}>
+              <FormLabel>E-mail address</FormLabel>
+              <TextField
+                aria-label="e-mail address"
+                type="email"
+                name="email"
+                required
+              />
+            </FormControl>
+            <FormControl margin="normal" error={!!formErrorMessage.confirmPassword} className={classes.fullWidth}>
+              <FormLabel>Password</FormLabel>
+              <TextField
+                aria-label="password"
+                type="password"
+                inputProps={{ minLength: 6 }}
+                required
+                name="password"
+              />
+              <FormHelperText>
+                {formErrorMessage.confirmPassword}
+              </FormHelperText>
+            </FormControl>
+            <FormControl margin="normal" error={!!formErrorMessage.confirmPassword} className={classes.fullWidth}>
+              <FormLabel>Confirm Password</FormLabel>
+              <TextField
+                aria-label="confirm password"
+                type="password"
+                inputProps={{ minLength: 6 }}
+                required
+                name="confirmPassword"
+              />
+              <FormHelperText className={classes.fullWidth}>
+                {formErrorMessage.confirmPassword}
+              </FormHelperText>
+            </FormControl>
+            <Button className={classes.formButton} type="submit" variant="contained" size="large">
               Create
             </Button>
           </Grid>
