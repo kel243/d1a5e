5df76d6dadb368cdd4a6c84ec2a8211d36c23bbf
Ken Lin <kel117@pitt.edu>
2022-04-22 09:48:05 -0400

    fixed db.js

--- server/db/db.js
+++ server/db/db.js
@@ -2,14 +2,9 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize(
-    process.env.DATABASE_URL || "messenger",
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD,
+    process.env.DATABASE_URL || `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:5433/messenger`,
     {
-        host: "localhost",
-        port: "5433",
         logging: false,
-        dialect: "postgres",
     }
 );
 
