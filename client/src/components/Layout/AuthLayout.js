befcd987e3f8260464b9a3c4d1a6f2eefa9605a1
Ken Lin <kel117@pitt.edu>
2022-04-22 09:37:41 -0400

    auth page layout component and styling

--- client/src/components/Layout/AuthLayout.js
+++ client/src/components/Layout/AuthLayout.js
@@ -11,27 +11,33 @@ import BackgroundImage from "../../assets/bg-img.png";
 const useStyles = makeStyles(() => ({
     root: {
         height: "100vh",
+        "@media (max-width: 786px)": {
+            gridTemplateColumns: '1fr',
+          }
     },
     background: {
         backgroundImage: `linear-gradient(180deg, rgb(58, 141, 255, .85) 0%, rgb(134, 185, 255, .85) 100%), url(${BackgroundImage})`,
         backgroundSize: 'cover',
         mixBlendMode: "normal",
         paddingTop: '32.5vh',
-        gap: '2rem'
+        gap: '2rem',
+        "@media (max-width: 786px)": {
+            display: 'none'
+          }
     },
     backgroundText: {
         fontSize: '1.625rem',
         color: '#fff',
         textAlign: 'center',
         width: '75%'
-    },
+    }
 }));
 
 const AuthLayout = ({children}) => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.root} display="grid" gridTemplateColumns="1fr 2fr">
+    <Box className={classes.root} display="grid" gridTemplateColumns="4.2fr 5.8fr">
         <Grid className={classes.background} container direction="column" alignItems='center'>
             <Bubble />
             <Typography className={classes.backgroundText}>
