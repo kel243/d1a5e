7075271c47f529bfb65b49703737d3a1242c4d8e
Ken Lin <kel117@pitt.edu>
2022-04-22 09:38:10 -0400

    login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,16 +2,19 @@ import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import {
   Grid,
-  Box,
   Typography,
   Button,
   FormControl,
   TextField,
+  FormLabel,
 } from '@material-ui/core';
 import AuthLayout from './components/Layout/AuthLayout';
+import useStyles from "./components/Layout/AuthPageStyles";
 
 const Login = ({ user, login }) => {
   const history = useHistory();
+
+  const classes = useStyles();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -29,41 +32,41 @@ const Login = ({ user, login }) => {
 
   return (
     <AuthLayout>
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Link href="/register" to="/register">
-            <Button>Register</Button>
+      <Grid container direction="column" justifyContent="center" alignItems="center">
+      <Grid className={classes.authSwitcher} container item alignItems='center' justifyContent='flex-end'>
+          <Typography className={classes.greyText}>Don't have an account?</Typography>
+          <Link href="/register" to="/register" className={classes.linkText}>
+            <Button className={classes.linkButton}>Create Account</Button>
           </Link>
         </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
+        <form className={classes.form} onSubmit={handleLogin}>
+        <Grid className={classes.formGrid} container direction='column' alignItems='center'>
+            <Typography className={classes.headingText} component="h1">Welcome back!</Typography>
+            <FormControl margin="normal" className={classes.fullWidth}>
+              <FormLabel>Username</FormLabel>
               <TextField
-                label="password"
+                aria-label="username"
+                name="username"
+                type="text"
+                required
+              />
+            </FormControl>
+            <FormControl margin="normal" className={classes.fullWidth}>
+              <FormLabel>Password</FormLabel>
+              <TextField
                 aria-label="password"
                 type="password"
                 name="password"
+                required
               />
+              <Button className={classes.forgotButton}>Forgot?</Button>
             </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
+            <Button className={classes.formButton} type="submit" variant="contained" size="large">
+              Login
+            </Button>
           </Grid>
         </form>
-      </Box>
+      </Grid>
     </AuthLayout>
   );
 };
